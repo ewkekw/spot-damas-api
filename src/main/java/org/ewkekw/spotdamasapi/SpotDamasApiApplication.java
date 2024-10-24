8cd52616eeadad0288aba592a5b801d2b237e7d7
kawe phelipe <kawepheliz@gmail.com>
2024-10-24 11:06:20 -0300

    Initial commit

---
+++ src/main/java/org/ewkekw/spotdamasapi/SpotDamasApiApplication.java
@@ -0,0 +1,13 @@
+package org.ewkekw.spotdamasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotDamasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotDamasApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ewkekw/spotdamasapi/SpotDamasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ewkekw.spotdamasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotDamasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
